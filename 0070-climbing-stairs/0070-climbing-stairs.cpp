560bdf0d79c48a441aa1d1afba5a4c1cbee09554
Sanvanth J R <168008238+Sanvanth@users.noreply.github.com>
2024-09-27 10:41:09 +0530

    Time: 0 ms (100%), Space: 7.4 MB (69.48%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,8 +3,8 @@ public:
     int climbStairs(int n) {
         int pre2 = 1;
         int pre1 = 1;
-       int i=2;
-       while( i <= n){
+       int i=1;
+       while( i < n){
             int next = pre1 +pre2;
             pre2=pre1;
             pre1 = next;
